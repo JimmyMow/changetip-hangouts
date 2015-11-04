d985059c1db3c89c0df31bd2553c84142a606b79
Jack Mallers <jimmymowschess@gmail.com>
2015-11-04 13:34:50 -0600

    debugging

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,27 +2,27 @@ function showParticipants() {
   var participants = gapi.hangout.getParticipants();
   console.log("participants: ", participants);
 
-  var ulTag = document.createElement('ul');
-  ulTag.id = "participantsList"
-  document.getElementById("participantsDiv").appendChild(ulTag);​
+  // var ulTag = document.createElement('ul');
+  // ulTag.id = "participantsList"
+  // document.getElementById("participantsDiv").appendChild(ulTag);​
 
-  for (var index in participants) {
-    var participant = participants[index];
-    console.log("participant: ", participant);
+  // for (var index in participants) {
+  //   var participant = participants[index];
+  //   console.log("participant: ", participant);
 
-    var listElement = document.createElement('li');
-    listElement.id = "person-"+participant.person.id
-    ​document.getElementById("participantsList").appendChild(listElement);​
+  //   var listElement = document.createElement('li');
+  //   listElement.id = "person-"+participant.person.id
+  //   ​document.getElementById("participantsList").appendChild(listElement);​
 
-    var aTag = document.createElement('a');
-    aTag.text = participant.person.displayName;
-    aTag.href = "#";
-    aTag.addEventListener('click', function(){
-      clickedPerson(participant);
-    });
+  //   var aTag = document.createElement('a');
+  //   aTag.text = participant.person.displayName;
+  //   aTag.href = "#";
+  //   aTag.addEventListener('click', function(){
+  //     clickedPerson(participant);
+  //   });
 
-    ​document.getElementById("person-"+participant.person.id).appendChild(aTag);​
-  }
+  //   ​document.getElementById("person-"+participant.person.id).appendChild(aTag);​
+  // }
 }
 
 function init() {
