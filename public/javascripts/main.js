ae740d445dcb957ac609ef3fb7f81314a88320e2
Jack Mallers <jimmymowschess@gmail.com>
2015-11-05 12:49:53 -0600

    added tip route with changetip functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,3 @@
-var ChangeTip = require('changetip');
-var change_tip = new ChangeTip({api_key: process.env.CHANGETIP_API_KEY});
-
 function showParticipants() {
   var participants = gapi.hangout.getParticipants();
   console.log("participants: ", participants);
@@ -43,9 +40,5 @@ function init() {
 
 function clickedPerson(participant) {
    console.log("here from clickedPerson with participant: ", participant);
-   console.log("key: ", process.env.CHANGETIP_API_KEY);
-   // {'receiver': '111750880711215438532', 'message': '/changetip @change here is a cent', 'sender': '108104158228107899864', 'meta': {'sender_display': 'Jack', 'receiver_display': 'Change'}, 'context_uid': 'b12dc3e8fd1a8626'}
-    change_tip.send_tip('b12dc3e8fd1a8626', '108104158228107899864', '111750880711215438532', 'googleplus', '@change here is a cent', {'sender_display': 'Jack', 'receiver_display': 'Change'}).then(function(result) {
-        console.log("results: ", results);
-    });
 }
+
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var ChangeTip = require('changetip');
+var change_tip = new ChangeTip({api_key: process.env.CHANGETIP_API_KEY});
+var md5 = require('MD5');
+var strftime = require('strftime');
+var crypto = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,8 +12,32 @@ router.get('/', function(req, res) {
 });
 
 router.get('/main', function(req, res) {
-  res.set('Content-Type', 'text/xml');
   res.render('main');
+});
+
+router.post('/tip', function(req, res) {
+   console.log("changetip: ", change_tip);
+   var sender = "108104158228107899864";
+   var receiver = '111750880711215438532';
+   var message = "@change here is a cent";
+   var post_data = "googleplus " + sender + ": " + message;
+   var encode_data = encodeURI(String(post_data));
+   var encode_date = encodeURI(strftime('%Y-%m-%d:%H:%M:00', new Date()));
+   var hash = crypto.createHash('md5')
+   .update(encode_data)
+   .update(encode_date)
+   .digest('hex');
+
+   change_tip.send_tip(hash,
+                       sender,
+                       receiver,
+                       'googleplus',
+                       '@change here is a cent',
+                       {'sender_display': 'Jack', 'receiver_display': 'Change'})
+   .then(function(result) {
+        console.log("result: ", result);
+        res.send({result: result});
+    });
 });
 
 module.exports = router;
