d5291cbef3cfa8114fb72f86aadb8ce4e5006001
Jack Mallers <jimmymowschess@gmail.com>
2015-11-11 20:37:44 -0600

    removed jsonp

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -35,8 +35,7 @@ $(document).ready(function() {
       data: realData,
       success: function(data) {
         console.log("data from ajax: ", data);
-      },
-      dataType: 'jsonp'
+      }
     });
   });
 });
