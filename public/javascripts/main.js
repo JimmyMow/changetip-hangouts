480e9c2d966b677f76c1b3b20e216f62b894fcca
Jack Mallers <jimmymowschess@gmail.com>
2015-11-11 19:55:14 -0600

    serializeObject method

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,38 @@
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 $(document).ready(function() {
   $("#sendTipForm").on("submit", function(e) {
     e.preventDefault();
-    console.log("form serialize: ", $(this).serialize());
+    console.log("form serialize: ", JSON.stringify($('#sendTipForm').serializeObject()));
+    $.fn.serializeObject = function() {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
     // var receiver = $('input[name=tip[receiver]]').val();
     // var sender = $('input[name=tip[receiver]]').val();
     // $.ajax({
