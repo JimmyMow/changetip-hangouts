421f59c1810ca38b4867c172ca660525506db89e
Jack Mallers <jimmymowschess@gmail.com>
2015-11-12 16:53:50 -0600

    using fetch instead of ajax

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -29,13 +29,16 @@ $(document).ready(function() {
       message: data['tip[money_val]']
     };
     console.log("real data: ", realData);
-    $.ajax({
-      type: "POST",
-      url: "https://stark-hamlet-6630.herokuapp.com/tip",
-      data: realData,
-      success: function(data) {
-        console.log("data from ajax: ", data);
-      }
+    fetch('https://stark-hamlet-6630.herokuapp.com/tip', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: realData
+    })
+    .then(function(data) {
+      console.log("data: ", data);
     });
   });
 });
